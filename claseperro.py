89249c8e3b4091774d35c643b5162d296e19e254
Luis Herrera <a.22308051281207@cbtis128.edu.mx>
2024-09-18 07:35:22 -0600

    Add files via upload

---
+++ claseperro.py
@@ -0,0 +1,17 @@
+print("--Programacion POO--")
+# definicion de clases
+class Perro:
+    # funciones dentro de la cla
+    def morder(self):
+        print("El perro me mordio")
+    def Datos_perro(self,nombre,edad):
+            print(f" Nombre : {nombre} edad : {edad}")
+
+
+# zona creacion de objetos
+pitbull=Perro()
+
+# zona de uso de objetos
+
+pitbull.Datos_perro("boby",4)
+pitbull.morder()
